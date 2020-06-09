4773ca83de1267468964420b471857a6af71e5cf
Romane <romane.bezier@epitech.eu>
2020-06-09 15:23:01 +0200

    Save alarm

--- app/src/main/java/com/example/prepreveil/MainActivity.kt
+++ app/src/main/java/com/example/prepreveil/MainActivity.kt
@@ -8,20 +8,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.format.DateUtils
 import android.text.format.Time
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
-import com.example.prepreveil.AlertReceiver
 import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 class MainActivity : AppCompatActivity(), OnTimeSetListener {
 
     companion object {
         lateinit var instance: MainActivity
             private set
+        var id = "1"
     }
 
     private var mTextView: TextView? = null
@@ -41,18 +44,43 @@ class MainActivity : AppCompatActivity(), OnTimeSetListener {
     }
 
     override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-        val c = Calendar.getInstance()
-        c[Calendar.HOUR_OF_DAY] = hourOfDay
-        c[Calendar.MINUTE] = minute
-        c[Calendar.SECOND] = 0
-        updateTimeText(c)
-        startAlarm(c)
+        if (view.isShown) {
+            val c = Calendar.getInstance()
+            c[Calendar.HOUR_OF_DAY] = hourOfDay
+            c[Calendar.MINUTE] = minute
+            c[Calendar.SECOND] = 0
+            updateTimeText(c)
+            startAlarm(c)
+        }
     }
 
     private fun updateTimeText(c: Calendar) {
         var timeText: String? = "Alarm set for: "
         timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.time)
         mTextView!!.text = timeText
+    }
+
+    private fun saveAlarm(time: Long) {
+
+        val sharedPref = this.getSharedPreferences("com.example.prepreveil", Context.MODE_PRIVATE)
+        sharedPref.edit().putLong(id, time).apply()
+        val newId = id.toInt() + 1
+        id = newId.toString()
+
+        for (i in 1..id.toInt()) {
+            val millis = sharedPref.getLong(i.toString(), 0)
+            // New date object from millis
+            // New date object from millis
+            val date = Date(millis)
+// formattter
+// formattter
+            val formatter = SimpleDateFormat("HH:mm:ss.SSS")
+            formatter.timeZone = TimeZone.getTimeZone("GMT+2")
+// Pass date object
+// Pass date object
+            val formatted: String = formatter.format(date)
+            Log.d("test", formatted)
+        }
     }
 
     private fun startAlarm(c: Calendar) {
@@ -63,6 +91,7 @@ class MainActivity : AppCompatActivity(), OnTimeSetListener {
             c.add(Calendar.DATE, 1)
         }
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.timeInMillis, pendingIntent)
+        saveAlarm(c.timeInMillis)
     }
 
     fun snoozeAlarm() {
@@ -76,6 +105,7 @@ class MainActivity : AppCompatActivity(), OnTimeSetListener {
         nextUpdateTime.set(nextUpdateTimeMillis)
 
         alarmManager.setExact(AlarmManager.RTC, nextUpdateTimeMillis, pendingIntent)
+        saveAlarm(nextUpdateTimeMillis)
     }
 
     fun cancelAlarm() {
